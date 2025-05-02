27544eaba4bbddc834efda3762ed9711b11b38dd
Lucas Phillipe Freitas Dias <lucas.pfd.2002@gmail.com>
2025-05-01 21:59:07 -0300

    Criando métodos no controller que são correspondentes aos métodos da service de transaction

--- src/main/java/com/bd/nosql/controller/TransactionController.java
+++ src/main/java/com/bd/nosql/controller/TransactionController.java
@@ -10,12 +10,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/transactions")
 public class TransactionController {
     @Autowired
     private TransactionService service;
+
+    @GetMapping
+    public ResponseEntity<List<TransactionResponseDto>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(service.getAllTransactions());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TransactionResponseDto> getTransactionById(@PathVariable UUID id){
+        return ResponseEntity.ok(service.getTransactionById(id));
+    }
+
+    @GetMapping("/orderByAmount")
+    public ResponseEntity<List<TransactionResponseDto>> getTransactionsOrderByAmount(@RequestParam int sort){
+        return ResponseEntity.ok(service.getTransactionsOrderByAmount(sort));
+    }
 
     @PostMapping("/add")
     public ResponseEntity<TransactionResponseDto> addTransaction(@RequestBody @Valid TransactionDto data) {
